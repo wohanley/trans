798a3fd084bc9cb1bdbaeb91000f8467ea4848b3
William O'Hanley <willy.ohanley@gmail.com>
2014-08-31 20:35:51 +1200

    a start on possibly

--- Trans.hs
+++ Trans.hs
@@ -3,9 +3,18 @@ module Trans(
 ) where
 
 import GHC.Word
+import System.Random
 
 transmit :: Word8 -> Word8
 transmit source = applyInversion source
+
+possibly :: (a -> a) -> Float -> a -> IO a
+possibly f probability x = do
+    gen <- newStdGen
+    let (rand, newGen) = random gen :: (Float, StdGen)
+    if rand < probability
+        then return (f x)
+        else return x
 
 applyInversion :: Word8 -> Word8
 applyInversion n = foldl invert n (powersOf2 8)
