4b7b2deaa2952690e41be2064c00d6d59674e3a3
William O'Hanley <willy.ohanley@gmail.com>
2014-09-01 15:56:20 +1200

    add flip errors (I'm not sure what these are actually called)

--- Trans.hs
+++ Trans.hs
@@ -7,8 +7,9 @@ import System.Random
 
 transmit :: StdGen -> Word8 -> Word8
 transmit gen source = 
-    let (rand, newGen) = random gen :: (Float, StdGen)
-    in possibly applyInversion rand 0.1 source
+    let (rand, gen') = random gen :: (Float, StdGen)
+        (rand', gen'') = random gen' :: (Float, StdGen)
+    in possibly applyFlipping rand 0.1 (possibly applyInversion rand' 0.1 source)
 
 possibly :: (Floating f, Ord f) => (a -> a) -> f -> f -> a -> a
 possibly f value threshold x = 
@@ -20,6 +21,16 @@ applyInversion n = foldl invert n (powersOf2 8)
 invert :: Word8 -> Word8 -> Word8
 invert n powerOf2 = if n >= powerOf2 then n - powerOf2
                     else n + powerOf2
+
+applyFlipping :: Word8 -> Word8
+applyFlipping n = foldl (Trans.flip (powersOf2 8)) n [0..5]
+
+flip :: [Word8] -> Word8 -> Int -> Word8
+flip powersOf2 n powerIndex =
+    invertAt (invertAt n powersOf2 powerIndex) powersOf2 (powerIndex + 2)
+
+invertAt :: Word8 -> [Word8] -> Int -> Word8
+invertAt n powersOf2 powerIndex = invert n (powersOf2 !! powerIndex)
 
 powersOf2 :: Word8 -> [Word8]
 powersOf2 n = map (pow 2) [1..n]
