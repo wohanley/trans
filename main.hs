18ba20ed6ec9e66c87feeeba5d59a1e21720fb8e
William O'Hanley <willy.ohanley@gmail.com>
2014-09-01 13:48:03 +1200

    getting randomized pretty well

--- main.hs
+++ main.hs
@@ -5,8 +5,12 @@ import GHC.Word
 
 main = do
     stream <- BS.getContents
+    mapM_ transform (unpack stream)
+
+transform :: Word8 -> IO ()
+transform source = do
     loadedTransmit <- genToTransmit
-    BS.putStr . BS.map loadedTransmit  $ stream
+    print . loadedTransmit $ source
 
 genToTransmit :: IO (Word8 -> Word8)
 genToTransmit = do
