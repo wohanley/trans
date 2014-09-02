445c93ec76aaab396ba97ac98605e9a088a8910c
William O'Hanley <willy.ohanley@gmail.com>
2014-09-02 18:03:28 +1200

    randomness reaching all the way in now

--- Trans.hs
+++ Trans.hs
@@ -5,30 +5,35 @@ module Trans
 import GHC.Word
 import System.Random
 
-errors :: (Floating f) => [((Word8 -> Word8), f)]
-errors =    [ (applyInversion, 0.01)
-            , (applyFlipping, 0.1)
+errors :: (Floating f) => [(StdGen, Word8) -> (StdGen, Word8)]
+errors =    [ (applyInversion 0.01)
+            --, (applyFlipping, 0.1)
             ]
-
-applyError :: (Word8, StdGen) -> ((Word8 -> Word8), Float) -> (Word8, StdGen)
-applyError (acc, gen) (error, probability) = 
-    let (rand, gen') = random gen :: (Float, StdGen)
-    in (possibly error rand probability acc, gen')
 
 transmit :: StdGen -> Word8 -> Word8
 transmit gen source = 
-    let (result, _) = foldl applyError (source, gen) errors
+    let (_, result) = foldl applyError (gen, source) errors
     in result
+
+applyError :: (StdGen, Word8) -> ((StdGen, Word8) -> (StdGen, Word8))
+    -> (StdGen, Word8)
+applyError (gen, acc) (error) = error (gen, acc)
 
 possibly :: (Floating f, Ord f) => (a -> a) -> f -> f -> a -> a
 possibly f value threshold x = 
     if value < threshold then f x else x
 
-applyInversion :: Word8 -> Word8
-applyInversion n = foldl invert n (powersOf2 8)
+applyInversion :: Float -> (StdGen, Word8) -> (StdGen, Word8)
+applyInversion probability (gen, n) =
+    foldl (inversionFoldFunction probability) (gen, n) (powersOf2 8)
+
+-- I want this in a where clause but I can't get the syntax to work
+inversionFoldFunction probability (gen, acc) powerOf2 =
+    let (rand, gen') = random gen :: (Float, StdGen)
+    in (gen', possibly (invert powerOf2) rand probability acc)
 
 invert :: Word8 -> Word8 -> Word8
-invert n powerOf2 = if n >= powerOf2 then n - powerOf2
+invert powerOf2 n = if n >= powerOf2 then n - powerOf2
                     else n + powerOf2
 
 applyFlipping :: Word8 -> Word8
@@ -39,7 +44,7 @@ flip powersOf2 n powerIndex =
     invertAt (invertAt n powersOf2 powerIndex) powersOf2 (powerIndex + 2)
 
 invertAt :: Word8 -> [Word8] -> Int -> Word8
-invertAt n powersOf2 powerIndex = invert n (powersOf2 !! powerIndex)
+invertAt n powersOf2 powerIndex = invert (powersOf2 !! powerIndex) n
 
 powersOf2 :: Word8 -> [Word8]
 powersOf2 n = [ pow 2 x | x <- [1..n] ]
